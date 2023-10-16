ccbfdb902fa118527f9d77ad6ccabe377da9f372
raghavk0077 <raghavkhanna1301@gmail.com>
2023-10-16 19:42:59 +0530

    Changes has been made accordingly to store the data in mysql from signupone page

--- src/atm/simulator/system/Conn.java
+++ src/atm/simulator/system/Conn.java
@@ -1,2 +1,20 @@
-package atm.simulator.system;public class Conn {
+package atm.simulator.system;
+
+import java.sql.*;
+
+// this class is created for the connection with the mysql database
+// as this class is used as the object in other classes for the connection
+public class Conn {
+
+    Connection c;
+    Statement s;
+    public Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "raghav");
+            s = c.createStatement();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
--- src/atm/simulator/system/SignupOne.java
+++ src/atm/simulator/system/SignupOne.java
@@ -3,13 +3,23 @@ package atm.simulator.system;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.awt.event.*;
 import com.toedter.calendar.JDateChooser;
-public class SignupOne extends JFrame {
+
+public class SignupOne extends JFrame implements ActionListener {
+
+    long random;
+    JTextField nameTextField, fnameTextField,  emailTextField, addressTextField,cityTextField, stateTextField, pincodeTextField;
+    JButton next;
+    JRadioButton male, female, married, single, other;
+    JDateChooser dateChooser;
+
     SignupOne(){
         //by default set layout is border layout so we have to null it so as to centralise it. and in border layout set bounds doesnt works
         setLayout(null);
         Random ran=new Random();
-        long random=Math.abs((ran.nextLong() % 9000L) + 1000L);
+        random=Math.abs((ran.nextLong() % 9000L) + 1000L);
+
 
         JLabel formno=new JLabel("APPLICATION FORM NO. " + random);
         formno.setFont(new Font("Raleway", Font.BOLD,38));
@@ -26,7 +36,7 @@ public class SignupOne extends JFrame {
         name.setBounds(100,140,100,30);
         add(name);
 
-        JTextField nameTextField=new JTextField();
+        nameTextField=new JTextField();
         nameTextField.setFont(new Font("Raleway",Font.BOLD,14));
         nameTextField.setBounds(300,140,400,30);
         add(nameTextField);
@@ -36,7 +46,7 @@ public class SignupOne extends JFrame {
         fname.setBounds(100,190,200,30);
         add(fname);
 
-        JTextField fnameTextField=new JTextField();
+        fnameTextField=new JTextField();
         fnameTextField.setFont(new Font("Raleway",Font.BOLD,14));
         fnameTextField.setBounds(300,190,400,30);
         add(fnameTextField);
@@ -46,7 +56,7 @@ public class SignupOne extends JFrame {
         dob.setBounds(100,240,200,30);
         add(dob);
 
-        JDateChooser dateChooser=new JDateChooser();
+        dateChooser=new JDateChooser();
         dateChooser.setBounds(300,240,400,30);
         dateChooser.setOpaque(true);
         dateChooser.setForeground(new Color(105,105,105));
@@ -57,12 +67,12 @@ public class SignupOne extends JFrame {
         gender.setBounds(100,290,200,30);
         add(gender);
 
-        JRadioButton male=new JRadioButton("Male");
+        male=new JRadioButton("Male");
         male.setBounds(300,290,90,30);
         male.setBackground(Color.WHITE);
         add(male);
 
-        JRadioButton female=new JRadioButton("Female");
+        female=new JRadioButton("Female");
         female.setBounds(450,290,120,30);
         female.setBackground(Color.WHITE);
         add(female);
@@ -76,7 +86,7 @@ public class SignupOne extends JFrame {
         email.setBounds(100,340,200,30);
         add(email);
 
-        JTextField emailTextField=new JTextField();
+        emailTextField=new JTextField();
         emailTextField.setFont(new Font("Raleway",Font.BOLD,14));
         emailTextField.setBounds(300,340,400,30);
         add(emailTextField);
@@ -87,18 +97,18 @@ public class SignupOne extends JFrame {
         add(status);
 
 
-        JRadioButton married=new JRadioButton("Married");
+        married=new JRadioButton("Married");
         married.setBounds(300,390,100,30);
         married.setBackground(Color.WHITE);
         add(married);
 
 
-        JRadioButton single=new JRadioButton("Unmarried");
+        single=new JRadioButton("Single");
         single.setBounds(450,390,100,30);
         single.setBackground(Color.WHITE);
         add(single);
 
-        JRadioButton other=new JRadioButton("Other");
+        other=new JRadioButton("Other");
         other.setBounds(630,390,100,30);
         other.setBackground(Color.WHITE);
         add(other);
@@ -113,7 +123,7 @@ public class SignupOne extends JFrame {
         address.setBounds(100,440,200,30);
         add(address);
 
-        JTextField addressTextField=new JTextField();
+        addressTextField=new JTextField();
         addressTextField.setFont(new Font("Raleway",Font.BOLD,14));
         addressTextField.setBounds(300,440,400,30);
         add(addressTextField);
@@ -123,7 +133,7 @@ public class SignupOne extends JFrame {
         city.setBounds(100,490,200,30);
         add(city);
 
-        JTextField cityTextField=new JTextField();
+        cityTextField=new JTextField();
         cityTextField.setFont(new Font("Raleway",Font.BOLD,14));
         cityTextField.setBounds(300,490,400,30);
         add(cityTextField);
@@ -133,7 +143,7 @@ public class SignupOne extends JFrame {
         state.setBounds(100,540,200,30);
         add(state);
 
-        JTextField stateTextField=new JTextField();
+        stateTextField=new JTextField();
         stateTextField.setFont(new Font("Raleway",Font.BOLD,14));
         stateTextField.setBounds(300,540,400,30);
         add(stateTextField);
@@ -143,18 +153,19 @@ public class SignupOne extends JFrame {
         pincode.setBounds(100,590,200,30);
         add(pincode);
 
-        JTextField pincodeTextField=new JTextField();
+        pincodeTextField=new JTextField();
         pincodeTextField.setFont(new Font("Raleway",Font.BOLD,14));
         pincodeTextField.setBounds(300,590,400,30);
         add(pincodeTextField);
 
-        JButton next=new JButton("Next");
+        next=new JButton("Next");
         next.setBorderPainted(false);
         next.setOpaque(true);
         next.setBackground(Color.BLACK);
         next.setForeground(Color.WHITE);
         next.setFont(new Font("Raleway",Font.BOLD,14));
         next.setBounds(620,660,80,30);
+        next.addActionListener(this);
         add(next);
 
 
@@ -163,6 +174,55 @@ public class SignupOne extends JFrame {
         setSize(850,800);
         setLocation(350,10);
         setVisible(true);
+    }
+
+    // when next button is clicked following action will be performed
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String formno = "" + random;
+        String name = nameTextField.getText();
+        String fname = fnameTextField.getText();
+        String dob = ((JTextField) dateChooser.getDateEditor().getUiComponent()).getText();
+        String gender = null;
+        if(male.isSelected()) {
+            gender = "Male";
+        }
+        else if (female.isSelected()) {
+            gender = "Female";
+        }
+        String email = emailTextField.getText();
+        String marital = null;
+        if(single.isSelected()) {
+            marital = "Single";
+        }
+        else if(married.isSelected()) {
+            marital = "Married";
+        }
+        else if(other.isSelected()) {
+            marital = "Other";
+        }
+        String address = addressTextField.getText();
+        String city = cityTextField.getText();
+        String state = stateTextField.getText();
+        String pincode = pincodeTextField.getText();
+
+        // as mysql is external entity we need to handle the exceptions
+        try {
+            if (name.equals("")) {
+                JOptionPane.showMessageDialog(null, "Name is Required");
+            }
+            // Here more form validation will be done later
+
+            // here the values has been added to the mysql database table
+            else {
+                Conn c = new Conn();
+                String query = "INSERT INTO Signup VALUES('"+formno+"','"+name+"','"+fname+"','"+dob+"','"+gender+"','"+email+"','"+marital+"','"+address+"','"+city+"','"+state+"','"+pincode+"')";
+                c.s.executeUpdate(query);
+            }
+        }
+        catch (Exception exception){
+            System.out.println(exception);
+        }
     }
     public static void main(String[] args) {
         //creating new object
