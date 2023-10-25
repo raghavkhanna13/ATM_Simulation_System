24638e32b7445ec43ad490e3a12b5c6eda1800fd
raghavk0077 <raghavkhanna1301@gmail.com>
2023-10-25 13:11:58 +0530

    Added more sql queries for third form and login

--- Sql_Queries.sql
+++ Sql_Queries.sql
@@ -3,6 +3,12 @@ show databases;
 use bankmanagementsystem;
 show tables;
 
+-- for login table
+create table login (FormNo varchar(10), Card_Number varchar(30), Pin varchar(10));
+select * from login;
+truncate login;
+
+
 -- for first table signupone table   
 create table signupone (FormNo varchar(10),Name varchar(30),Father_name varchar(30),DOB varchar(20),Gender varchar(10),Email varchar(40),Marital_Status varchar(10),Address varchar(100),City varchar(20),State varchar(20),Pincode varchar(10));
 select * from signupone;
@@ -13,6 +19,11 @@ truncate  signupone;
 create table signuptwo (FormNo varchar(10), Religion varchar(30),Category varchar(30),Income varchar(30),Education_Qualification varchar(30),Occupation varchar(20),PAN_Number varchar(20),Aadhar_Number varchar(20),Senior_Citizen varchar(10),Existing_Account varchar(10));   
 select * from signuptwo;
 truncate signuptwo;
+
+-- for table third signupthree
+create table signupthree (FormNo varchar(10), Account_Type varchar(30),Card_Number varchar(30),Pin varchar(10),Facilities varchar(150));
+select * from signupthree;
+truncate signupthree;   
 
 
 
